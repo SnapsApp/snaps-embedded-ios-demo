b156eefe14f4b2b1a48d4f04b4ceae1b9209ed6a
fisch0920 <fisch0920@gmail.com>
2014-09-22 15:45:31 -0400

    adding save image functionality

---
+++ SnapsEmbeddedDemo/JSTools.js
@@ -0,0 +1,25 @@
+
+function MyAppGetHTMLElementsAtPoint (x, y) {
+    var tags = ","
+    var e = document.elementFromPoint(x, y)
+    while (e) {
+        if (e.tagName) {
+            tags += e.tagName + ','
+        }
+        e = e.parentNode
+    }
+    return tags
+}
+
+function MyAppGetLinkSRCAtPoint (x, y) {
+    var tags = ""
+    var e = document.elementFromPoint(x, y)
+    while (e) {
+        if (e.src) {
+            tags += e.src
+            break
+        }
+        e = e.parentNode
+    }
+    return tags
+}
--- SnapsEmbeddedDemo/SNViewController.m
+++ SnapsEmbeddedDemo/SNViewController.m
@@ -8,9 +8,11 @@
 
 #import "SNViewController.h"
 
-@interface SNViewController () <UIWebViewDelegate>
+@interface SNViewController () <UIWebViewDelegate, UIActionSheetDelegate>
 
 @property (nonatomic, strong) UIImageView *_imageView;
+@property (nonatomic, strong) UIWebView *_webView;
+@property (nonatomic, strong) NSString *_selectedImageURL;
 
 @end
 
@@ -18,16 +20,28 @@
 
 - (IBAction)launchSnaps:(id)sender
 {
-    UIWebView *webView = [[UIWebView alloc] initWithFrame:self.view.frame];
-    webView.delegate = self;
-    [self.view addSubview:webView];
-    [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://staging.makesnaps.com/rockets"]]];
+    self._webView = [[UIWebView alloc] initWithFrame:self.view.frame];
+    self._webView.delegate = self;
+    [self.view addSubview:self._webView];
+    [self._webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://staging.makesnaps.com/rockets"]]];
+    
+    UILongPressGestureRecognizer *tapAndHold = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPress:)];
+    [self._webView addGestureRecognizer:tapAndHold];
+}
+
+- (void)handleLongPress:(UILongPressGestureRecognizer*)sender
+{
+    if (sender.state == UIGestureRecognizerStateEnded) {
+        [self openContextualMenuAt:[sender locationInView:self._webView]];
+    }
 }
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView
 {
     NSString *javascriptString = [NSString stringWithFormat:@"setHostApp(\"%@\")", @"ios"];
     [webView stringByEvaluatingJavaScriptFromString:javascriptString];
+    [webView stringByEvaluatingJavaScriptFromString:@"document.body.style.webkitTouchCallout='none';"];
+    [webView stringByEvaluatingJavaScriptFromString:@"document.body.className+=' unselectable';"];
 }
 
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
@@ -56,6 +70,54 @@
     }
     
     return true;
+}
+
+- (void)openContextualMenuAt:(CGPoint)pt
+{
+    // Load the JavaScript code from the Resources and inject it into the web page
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"JSTools" ofType:@"js"];
+    NSString *jsCode = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+    [self._webView stringByEvaluatingJavaScriptFromString:jsCode];
+    
+    // get the Tags at the touch location
+    NSString *tags = [self._webView stringByEvaluatingJavaScriptFromString:
+                      [NSString stringWithFormat:@"MyAppGetHTMLElementsAtPoint(%i,%i);",(NSInteger)pt.x,(NSInteger)pt.y]];
+    
+    NSString *tagsSRC = [self._webView stringByEvaluatingJavaScriptFromString:
+                         [NSString stringWithFormat:@"MyAppGetLinkSRCAtPoint(%i,%i);",(NSInteger)pt.x,(NSInteger)pt.y]];
+    
+    self._selectedImageURL = @"";
+    
+    // If an image was touched, add image-related buttons.
+    if ([tags rangeOfString:@",IMG,"].location != NSNotFound && [tagsSRC rangeOfString:@"data:image"].location == NSNotFound) {
+        self._selectedImageURL = tagsSRC;
+        
+        UIActionSheet *sheet = [[UIActionSheet alloc] initWithTitle:nil delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles:nil];
+        if (sheet.title == nil) {
+            sheet.title = @"Snap";
+        }
+        
+        [sheet addButtonWithTitle:@"Save Image"];
+        [sheet addButtonWithTitle:@"Copy Image"];
+        
+        [sheet showInView:self.view];
+    }
+}
+
+- (void)actionSheet:(UIActionSheet*)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if ([[actionSheet buttonTitleAtIndex:buttonIndex] isEqualToString:@"Copy Image"]) {
+        [[UIPasteboard generalPasteboard] setString:self._selectedImageURL];
+    } else if ([[actionSheet buttonTitleAtIndex:buttonIndex] isEqualToString:@"Save Image"]) {
+        NSOperationQueue *queue = [NSOperationQueue new];
+        NSInvocationOperation *operation = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(saveImageURL:) object:self._selectedImageURL];
+        [queue addOperation:operation];
+    }
+}
+
+-(void)saveImageURL:(NSString*)url
+{
+    UIImageWriteToSavedPhotosAlbum([UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:url]]], nil, nil, nil);
 }
 
 @end
